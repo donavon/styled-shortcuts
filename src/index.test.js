0065f433b393f6bed41650902be634589ff4358f
Wes Moberly <github.wes@mailhero.io>
2018-05-14 12:41:05 -0400

    Clean up spacing in unit tests, consolidate test helpers

--- src/index.test.js
+++ src/index.test.js
@@ -17,16 +17,18 @@ describe('withStyledShortcuts', () => {
   test('is a function', () => {
     expect(typeof withStyledShortcuts).toBe('function');
   });
+
   test('that returns a function', () => {
     const data = withStyledShortcuts(mock);
     expect(typeof data).toBe('function');
   });
 });
 
-const testStyled = (mockStyled) => {
+const testTemplateFn = (mockStyled) => {
   test('is a function', () => {
     expect(typeof mockStyled).toBe('function');
   });
+
   test('calling that function returns { [strings], [values] }', () => {
     const strings = ['foo'];
     const data = mockStyled(strings, 'bar');
@@ -58,6 +60,7 @@ const testStyled = (mockStyled) => {
     const prop = fn({ bar: 'bar' });
     expect(prop).toBe('bar');
   });
+
   test('calling one of these functions with props, will return correct prop (w/units)', () => {
     const strings = ['foo'];
     const data = mockStyled(strings, 'bar:unit');
@@ -65,6 +68,7 @@ const testStyled = (mockStyled) => {
     const prop = fn({ bar: 'baz' });
     expect(prop).toBe('bazunit');
   });
+
   test('calling one of these functions with dotted props, will return correct prop', () => {
     const strings = ['foo'];
     const data = mockStyled(strings, 'a.b');
@@ -72,6 +76,7 @@ const testStyled = (mockStyled) => {
     const prop = fn({ a: { b: 'c' } });
     expect(prop).toBe('c');
   });
+
   test('calling one of these functions with dotted props, will return correct prop (w/units)', () => {
     const strings = ['foo'];
     const data = mockStyled(strings, 'a.b:unit');
@@ -79,6 +84,7 @@ const testStyled = (mockStyled) => {
     const prop = fn({ a: { b: 'c' } });
     expect(prop).toBe('cunit');
   });
+
   test('calling one of these functions with props, will return correct prop (num w/units)', () => {
     const strings = ['foo'];
     const data = mockStyled(strings, 'bar:unit');
@@ -86,6 +92,7 @@ const testStyled = (mockStyled) => {
     const prop = fn({ bar: 42 });
     expect(prop).toBe('42unit');
   });
+
   test('calling one of these functions with props, will return correct prop (0 w/units)', () => {
     const strings = ['foo'];
     const data = mockStyled(strings, 'bar:unit');
@@ -95,29 +102,27 @@ const testStyled = (mockStyled) => {
   });
 };
 
-const testTemplateFactoryFns = (mockStyled) => {
+const testStyled = (mockStyled) => {
   TEMPLATE_FACTORY_FN_NAMES.forEach((fnName) => {
     const templateFn = mockStyled[fnName]();
     const chainedTemplateFn = templateFn[fnName]();
 
     describe(`has \`${fnName}()\` function that returns a template function`, () => {
-      testStyled(templateFn);
+      testTemplateFn(templateFn);
     });
 
     describe(`has \`${fnName}()\` function that is chainable`, () => {
-      testStyled(chainedTemplateFn);
+      testTemplateFn(chainedTemplateFn);
     });
   });
 };
 
 describe('The new wrapped object', () => {
   describe('Wrapping a Component ie: wrappedStyled(Component)', () => {
-    testStyled(wrappedStyled());
-    testTemplateFactoryFns(wrappedStyled());
+    testStyled(wrappedStyled({}));
   });
 
   describe('A pre-defined HTML element ie: wrappedStyled.div', () => {
     testStyled(wrappedStyled.div);
-    testTemplateFactoryFns(wrappedStyled.div);
   });
 });
